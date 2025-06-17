52e4427778c8c98d0cdab3b8c1bab6565c95b96a
FelopaterAdel <felopateradel@yahoo.com>
2025-06-17 21:14:29 +0300

    update index

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require("express");
+import express from'express';
+import mongoose from'mongoose';
+import authRoutes from './routes/authRoute.js'
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 const app = express();
-const port = 3000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, Node.js Project!");
 });
+app.use('/auth',authRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => app.listen(process.env.PORT, () => console.log('Server running on port 3000')))
+.catch(err => console.log(err));
