c695348a4f271062d6172a8bbf1d303e93e1a53d
FelopaterAdel <felopateradel@yahoo.com>
2025-06-17 21:12:00 +0300

    Add  login and Registration in  authController

---
+++ controllers/authController.js
@@ -0,0 +1,68 @@
+import { catchAsync } from '../utils/wrapperFunction.js';
+import User  from '../models/userModel.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+
+export const register =catchAsync(async (req, res) => {
+    
+    const { name, email,telephone ,password, role } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+    const user = new User({
+      name,
+      email,
+      telephone,
+      password,
+      role
+    });
+
+    await user.save();
+
+    res.status(201).json({ message: 'User created successfully' });
+  
+});
+export const login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).json({
+      status: "fail",
+      message: "Please provide email and password"
+    });
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+  if (!user) {
+    return res.status(401).json({
+      status: "fail",
+      message: "Invalid email "
+    });
+  }
+  console.log(user.password);
+   const isValid =user.comparePassword(password);
+  if (!isValid) {
+    return res.status(401).json({
+      status: "fail",
+      message: "Invalid  password"
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+      role: user.role
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+
+  res.status(200).json({
+    status: "success",
+    token: token
+  });
+});
