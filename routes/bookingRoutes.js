b55713027b05aa7cb08e3371e3083a81b1e4e07d
Mohamed Mustafa <mohamedmustafa8112001@gmail.com>
2025-06-23 16:53:04 +0300

    Add get all times

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { addAvailableTime } from "../controllers/bookingController.js";
+import { addAvailableTime ,getAvailableTimesForShop} from "../controllers/bookingController.js";
 import {protect} from "../middlewares/protect.js"
 const router = express.Router();
 
 router.post('/', protect, addAvailableTime);
+router.get('/:shopId', protect, getAvailableTimesForShop);
 
 export default router;
