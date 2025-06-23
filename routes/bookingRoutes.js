72042a4167e7dd461c33573707416d7c652f0c9f
Mohamed Mustafa <mohamedmustafa8112001@gmail.com>
2025-06-23 17:46:50 +0300

    Add book route

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { addAvailableTime ,getAvailableTimesForShop} from "../controllers/bookingController.js";
-import {protect} from "../middlewares/protect.js"
+import {
+  addAvailableTime,
+  getAvailableTimesForShop,
+  bookTime,
+} from "../controllers/bookingController.js";
+import { protect } from "../middlewares/protect.js";
 const router = express.Router();
 
-router.post('/', protect, addAvailableTime);
-router.get('/:shopId', protect, getAvailableTimesForShop);
+router.post("/", protect, addAvailableTime);
+router.get("/:shopId", protect, getAvailableTimesForShop);
+router.post("/book", protect, bookTime);
 
 export default router;
