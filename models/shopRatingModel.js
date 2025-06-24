e14f741a8a9622348c532d3ddd158802e8cce6fa
FelopaterAdel <felopateradel@yahoo.com>
2025-06-24 15:30:56 +0300

    add rate model

---
+++ models/shopRatingModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const ShopRatingSchema = new mongoose.Schema({
+  shop: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Shop',
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+   // required: true
+  },
+  comment: {
+    type: String,
+    maxlength: 500
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const ShopRating = mongoose.model("ShopRating", ShopRatingSchema);
+
+export default ShopRating;
