c1b5ff91072b975401f67872e79c3fb38246fda4
MoatazNoaman <moataz.noaman12@gmail.com>
2025-06-28 17:17:20 +0300

    add fav models, controller, route

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,6 @@
 import { catchAsync } from "../utils/wrapperFunction.js";
 import Product from "../models/productModel.js";
+import Favorite from "../models/FavModels.js";
 
 export const createProduct = catchAsync(async (req, res) => {
   const {
@@ -65,3 +66,66 @@ export const deletedProduct = catchAsync(async (req, res) => {
   }
   res.status(200).json({ status: "success", message: "Product deleted" });
 });
+
+
+export const addToFav = catchAsync(async (req, res) => {
+  const { userId, productId} = req.body;
+
+  const existingFavorite = await Favorite.findOne({ 
+    user: userId, 
+    product: productId 
+  });
+
+  if (existingFavorite) {
+    return res.status(400).json({
+      status: "Already exists",
+      message: "Product already in favorites"
+    });
+  }
+
+  const newFavorite = await Favorite.create({
+    user: userId,
+    product: productId
+  });
+
+  res.status(201).json({
+    status: "Added", 
+    message: "Product added to favorites",
+    data: newFavorite
+  });
+})
+
+export const getAllFav = catchAsync(async (req, res)=> {
+  const {id} = req.params;
+  const favorites = await Favorite.find({ user: id })
+  .populate('product')
+  .exec()
+
+  return res.status(200).json({
+    status: "success", 
+    results: favorites.length,
+    data: { favorites }
+  })
+})
+
+export const removeFromFav = catchAsync(async (req, res) => {
+  const { userId, productId } = req.body;
+
+  // Find and delete the favorite entry
+  const deletedFavorite = await Favorite.findOneAndDelete({
+    user: userId,
+    product: productId
+  });
+
+  if (!deletedFavorite) {
+    return res.status(404).json({
+      status: "Not Found",
+      message: "Product not found in favorites"
+    });
+  }
+
+  res.status(200).json({
+    status: "Removed", 
+    message: "Product removed from favorites"
+  });
+})
